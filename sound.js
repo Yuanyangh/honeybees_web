ab605cf3a03f0ba88ac11b608345091bc56ba888
bluesnooker123 <dev333perfect@gmail.com>
2024-03-14 16:20:56 +0800

    update for sound icon

--- sound.js
+++ sound.js
@@ -1,6 +1,17 @@
+$(document).ready(function () {
+  
+  $(".music-icon").click(function(){
+    if ( $(".music-icon").hasClass("playing") ) {
+      $(".music-icon").removeClass("playing")
+    } else {
+      $(".music-icon").addClass("playing")
+    }
+  })
+
+})
+
 function toggleSound() {
   var audio = document.getElementById("myAudio");
-
   if (audio.paused) {
     audio.play();
   } else {
