6ed9957cd4ab7a459c5225695314e0225ec36db8
bluesnooker123 <dev333perfect@gmail.com>
2024-03-22 16:46:48 +0800

    update for Mobile Responsive

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -9,7 +9,27 @@ $(document).ready(function () {
 });
 
 window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  var lorem_dialog = document.getElementById("lorem_first");
+  if (event.target == lorem_dialog) {
+    lorem_dialog.style.display = "none";
+  }
 };
+
+function toggleDialog(dialogId) {
+  var windowWidth = window.innerWidth;
+  console.log(windowWidth);
+  if (windowWidth >= 500) {
+    return false;
+  } else {
+    var dialog = document.getElementById(dialogId);
+    if (dialog.style.display === "block") {
+      dialog.style.display = "none";
+    } else {
+      dialog.style.display = "block";
+    }
+  }
+}
