3e55c76705c6faaedc51d865a493cac3f70041ad
bluesnooker123 <dev333perfect@gmail.com>
2024-03-26 10:45:43 +0800

    implement expanded images for Mobile on Home page

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -13,9 +13,29 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-  var lorem_dialog = document.getElementById("lorem_first");
-  if (event.target == lorem_dialog) {
-    lorem_dialog.style.display = "none";
+  var lorem_first_dialog = document.getElementById("lorem_first");
+  if (event.target == lorem_first_dialog) {
+    lorem_first_dialog.style.display = "none";
+  }
+  var lorem_second_dialog = document.getElementById("lorem_second");
+  if (event.target == lorem_second_dialog) {
+    lorem_second_dialog.style.display = "none";
+  }
+  var lorem_third_dialog = document.getElementById("lorem_third");
+  if (event.target == lorem_third_dialog) {
+    lorem_third_dialog.style.display = "none";
+  }
+  var event_first = document.getElementById("event_section_first");
+  if (event.target == event_first) {
+    event_first.style.display = "none";
+  }
+  var event_second = document.getElementById("event_section_second");
+  if (event.target == event_second) {
+    event_second.style.display = "none";
+  }
+  var event_third = document.getElementById("event_section_third");
+  if (event.target == event_third) {
+    event_third.style.display = "none";
   }
 };
 
