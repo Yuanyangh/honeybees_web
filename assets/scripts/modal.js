12ad51a44075736fb7299aa87df0c1f3776a407d
bluesnooker123 <dev333perfect@gmail.com>
2024-03-27 19:47:59 +0800

    update for mobile responsive according to feedback

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -2,9 +2,14 @@ $(document).ready(function () {
   var modal = document.getElementById("myModal");
   $("#openModalBtn").click(function () {
     modal.style.display = "block";
+    modal.querySelector(".modal-content").style.animationName = "slideUp";
   });
   $(".close-nav-modal").click(function () {
-    modal.style.display = "none";
+    // modal.style.display = "none";
+    modal.querySelector(".modal-content").style.animationName = "slideDown";
+    setTimeout(() => {
+      modal.style.display = "none";
+    }, 500);
   });
 });
 
@@ -13,30 +18,30 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-  var lorem_first_dialog = document.getElementById("lorem_first");
-  if (event.target == lorem_first_dialog) {
-    lorem_first_dialog.style.display = "none";
-  }
-  var lorem_second_dialog = document.getElementById("lorem_second");
-  if (event.target == lorem_second_dialog) {
-    lorem_second_dialog.style.display = "none";
-  }
-  var lorem_third_dialog = document.getElementById("lorem_third");
-  if (event.target == lorem_third_dialog) {
-    lorem_third_dialog.style.display = "none";
-  }
-  var event_first = document.getElementById("event_section_first");
-  if (event.target == event_first) {
-    event_first.style.display = "none";
-  }
-  var event_second = document.getElementById("event_section_second");
-  if (event.target == event_second) {
-    event_second.style.display = "none";
-  }
-  var event_third = document.getElementById("event_section_third");
-  if (event.target == event_third) {
-    event_third.style.display = "none";
-  }
+  // var lorem_first_dialog = document.getElementById("lorem_first");
+  // if (event.target == lorem_first_dialog) {
+  //   lorem_first_dialog.style.display = "none";
+  // }
+  // var lorem_second_dialog = document.getElementById("lorem_second");
+  // if (event.target == lorem_second_dialog) {
+  //   lorem_second_dialog.style.display = "none";
+  // }
+  // var lorem_third_dialog = document.getElementById("lorem_third");
+  // if (event.target == lorem_third_dialog) {
+  //   lorem_third_dialog.style.display = "none";
+  // }
+  // var event_first = document.getElementById("event_section_first");
+  // if (event.target == event_first) {
+  //   event_first.style.display = "none";
+  // }
+  // var event_second = document.getElementById("event_section_second");
+  // if (event.target == event_second) {
+  //   event_second.style.display = "none";
+  // }
+  // var event_third = document.getElementById("event_section_third");
+  // if (event.target == event_third) {
+  //   event_third.style.display = "none";
+  // }
 };
 
 function toggleDialog(dialogId) {
