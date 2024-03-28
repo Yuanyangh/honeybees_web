ea2c1d2f627aa8fa0e8f74c03c332e11214a9ef9
bluesnooker123 <dev333perfect@gmail.com>
2024-03-28 16:26:39 +0800

    update css

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -22,26 +22,6 @@ window.onclick = function (event) {
   // if (event.target == lorem_first_dialog) {
   //   lorem_first_dialog.style.display = "none";
   // }
-  // var lorem_second_dialog = document.getElementById("lorem_second");
-  // if (event.target == lorem_second_dialog) {
-  //   lorem_second_dialog.style.display = "none";
-  // }
-  // var lorem_third_dialog = document.getElementById("lorem_third");
-  // if (event.target == lorem_third_dialog) {
-  //   lorem_third_dialog.style.display = "none";
-  // }
-  // var event_first = document.getElementById("event_section_first");
-  // if (event.target == event_first) {
-  //   event_first.style.display = "none";
-  // }
-  // var event_second = document.getElementById("event_section_second");
-  // if (event.target == event_second) {
-  //   event_second.style.display = "none";
-  // }
-  // var event_third = document.getElementById("event_section_third");
-  // if (event.target == event_third) {
-  //   event_third.style.display = "none";
-  // }
 };
 
 function toggleDialog(dialogId) {
