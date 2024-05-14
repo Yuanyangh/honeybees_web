c98c9dd7ffcba354b995679f13ee025eb92ec623
bluesnooker123 <dev333perfect@gmail.com>
2024-05-14 17:52:59 +0800

    fix scroll issue on Career page

--- assets/scripts/career.js
+++ assets/scripts/career.js
@@ -27,52 +27,16 @@ $(document).ready(async function() {
     path: "./assets/lotties/scrolldown.json" // the path to the animation json
   });
 
-  mouseLottieAnimation.play();
+  mouseLottieAnimation2.play();
 
   var initialOffset = $("#dedicat_mentor").offset().top;
   var initialOffset2 = $("#digital_ecosys").offset().top;
   var initialOffset3 = $("#process_excel").offset().top;
 
-  window.onscroll = function() {
-    var scrollPosition =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    var dedicat_mentor = document.getElementById("dedicat_mentor");
-    var digital_ecosys = document.getElementById("digital_ecosys");
-    var process_excel = document.getElementById("process_excel");
-    if (scrollPosition > initialOffset) {
-      dedicat_mentor.classList.add("overlayer_fixed");
-      digital_ecosys.style.marginTop = `${dedicat_mentor.offsetHeight -
-        (scrollPosition - initialOffset)}px`;
-    } else if (scrollPosition) {
-      dedicat_mentor.classList.remove("overlayer_fixed");
-    }
-    if (
-      scrollPosition >
-      (initialOffset3 + initialOffset2 + initialOffset) / 3
-    ) {
-      dedicat_mentor.style.marginTop = "0px";
-      digital_ecosys.style.marginTop = "0px";
-    } else if (scrollPosition > (initialOffset2 + initialOffset) / 2) {
-      digital_ecosys.classList.add("overlayer_fixed");
-      process_excel.style.marginTop = `${digital_ecosys.offsetHeight -
-        (scrollPosition - initialOffset2)}px`;
-      digital_ecosys.style.marginTop = "0px";
-    } else if (scrollPosition) {
-      digital_ecosys.classList.remove("overlayer_fixed");
-    }
-    if (
-      scrollPosition >
-      (initialOffset3 + initialOffset2 + initialOffset) / 3
-    ) {
-      dedicat_mentor.style.marginTop = "0px";
-      digital_ecosys.style.marginTop = "0px";
-    }
-  };
-
   $(window).scroll(function() {
     // calculate the percentage of the window scroll.
-    var scroll = $(window).scrollTop(),
-      height = $(window).height();
+    var scroll = $(window).scrollTop();
+    var height = $(window).height();
 
     if (scroll / height < 0.5) {
       $(".top-scale-1").addClass("fadeIn");
@@ -95,6 +59,38 @@ $(document).ready(async function() {
       $(".top-scale-2").removeClass("fadeIn");
       $(".top-scale-3").addClass("fadeIn");
       $(".top-scale-3").removeClass("fadeOut");
+    }
+
+    var dedicat_mentor = document.getElementById("dedicat_mentor");
+    var digital_ecosys = document.getElementById("digital_ecosys");
+    var process_excel = document.getElementById("process_excel");
+    if (scroll > initialOffset) {
+      dedicat_mentor.classList.add("overlayer_fixed");
+      digital_ecosys.style.marginTop = `${dedicat_mentor.offsetHeight -
+        (scroll - initialOffset)}px`;
+    } else if (scroll) {
+      dedicat_mentor.classList.remove("overlayer_fixed");
+    }
+    if (
+      scroll >
+      (initialOffset3 + initialOffset2 + initialOffset) / 3
+    ) {
+      dedicat_mentor.style.marginTop = "0px";
+      digital_ecosys.style.marginTop = "0px";
+    } else if (scroll > (initialOffset2 + initialOffset) / 2) {
+      digital_ecosys.classList.add("overlayer_fixed");
+      process_excel.style.marginTop = `${digital_ecosys.offsetHeight -
+        (scroll - initialOffset2)}px`;
+      digital_ecosys.style.marginTop = "0px";
+    } else if (scroll) {
+      digital_ecosys.classList.remove("overlayer_fixed");
+    }
+    if (
+      scroll >
+      (initialOffset3 + initialOffset2 + initialOffset) / 3
+    ) {
+      dedicat_mentor.style.marginTop = "0px";
+      digital_ecosys.style.marginTop = "0px";
     }
   });
 });
