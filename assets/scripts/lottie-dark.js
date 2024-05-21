389200fef9330129c84dcf9aaf7ec9de8b71eb86
bluesnooker123 <dev333perfect@gmail.com>
2024-05-21 21:30:59 +0800

    initial commit for `BEES ON THE SEA` event page

--- assets/scripts/lottie-dark.js
+++ assets/scripts/lottie-dark.js
@@ -1,10 +1,18 @@
 $(document).ready(async function () {
+  var originPath = window.location.origin; // Get the root URL
+  var subdirPath;
+  if ( window.location.pathname.includes('events') )
+    subdirPath = window.location.pathname.split('/').slice(0, -2).join('/'); 
+  else
+    subdirPath = window.location.pathname.split('/').slice(0, -1).join('/'); 
+
+  var lottiePath = `${originPath}${subdirPath}/assets/lotties/sound-dark/sound.json`;
   var animation = lottie.loadAnimation({
     container: document.getElementById("lottie-sound"), // the dom element
     renderer: "svg",
     loop: true,
     autoplay: false,
-    path: "./assets/lotties/sound-dark/sound.json", // the path to the animation json
+    path: lottiePath, // the path to the animation json
   });
 
   animation.play();
