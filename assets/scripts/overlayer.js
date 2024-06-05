1d1b6dc3fdd62811e9d9220a8b71f0343feae77f
bluesnooker123 <dev333perfect@gmail.com>
2024-06-05 11:41:26 +0800

    do not show banner on Home page

--- assets/scripts/overlayer.js
+++ assets/scripts/overlayer.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 
     if (scroll > initialOffset) {
       happenContainer.classList.add("overlayer_happen_fixed");
-      loremSection.style.marginTop = `${happenContainer.offsetHeight}px`;
+      loremSection.style.marginTop = `${happenContainer.offsetHeight + 170 - 30 - 50}px`; 
+      // 170: height of `index-happen-marquee`, 30: index-happen-marquee have margin-top: -30px, 50: the parent div of loren_sectiom have margin-top: 50px
     } else if ( scroll ) {
       happenContainer.classList.remove("overlayer_happen_fixed");
       loremSection.style.marginTop = "0";
