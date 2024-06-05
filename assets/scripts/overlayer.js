8e46bb795a211f3aff76b77fb544b70de6c776f8
bluesnooker123 <dev333perfect@gmail.com>
2024-06-05 11:02:53 +0800

    fix scroll issue, remove second overlay effect on Home page

--- assets/scripts/overlayer.js
+++ assets/scripts/overlayer.js
@@ -1,34 +1,18 @@
 $(document).ready(function() {
   var initialOffset = $("#happen-container").offset().top;
-  var initialOffset2 = $("#holdon-content").offset().top;
 
   $(window).on("scroll", function() {
-    var scrollPosition =
-      document.documentElement.scrollTop || document.body.scrollTop;
+    var scroll = $(window).scrollTop();
     var happenContainer = document.getElementById("happen-container");
     var loremSection = document.getElementById("lorem-section");
-    var eventSection = document.getElementById("event-section");
-    var holdonContent = document.getElementById("holdon-content");
 
-    if (scrollPosition > initialOffset) {
+    if (scroll > initialOffset) {
       happenContainer.classList.add("overlayer_happen_fixed");
-      loremSection.style.marginTop = `${initialOffset -
-        (scrollPosition - initialOffset)}px`;
-    } else {
+      loremSection.style.marginTop = `${happenContainer.offsetHeight}px`;
+    } else if ( scroll ) {
       happenContainer.classList.remove("overlayer_happen_fixed");
+      loremSection.style.marginTop = "0";
     }
 
-    if (scrollPosition > initialOffset2 - holdonContent.offsetHeight) {
-      loremSection.classList.add("overlayer_holdon_fixed");
-      eventSection.style.marginTop = `${initialOffset2 -
-        scrollPosition +
-        holdonContent.offsetHeight +
-        $(window).height()}px`;
-
-      // happenContainer.classList.remove("overlayer_happen_fixed");
-      loremSection.style.marginTop = "0px";
-    } else {
-      loremSection.classList.remove("overlayer_holdon_fixed");
-    }
   });
 });
