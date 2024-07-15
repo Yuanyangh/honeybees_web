679c45dd87992dad7de9042560e5fd3580a8a9d9
bluesnooker123 <dev333perfect@gmail.com>
2024-07-15 17:24:45 +0800

    update css on AboutUs page

--- about.js
+++ about.js
@@ -31,7 +31,19 @@ $(document).ready(function () {
   }
   carousels();
 
+  function showItems(el) {
+    var windowHeight = $(window).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("fade-in-up");
+        }
+    });
+  }  
+
   $(window).scroll(function() {
+    showItems('.corporate-sec');
     // calculate the percentage of the window scroll.
     var scroll = $(window).scrollTop();
     var initialOffset = $("#aboutus_fixed_container").offset().top;
