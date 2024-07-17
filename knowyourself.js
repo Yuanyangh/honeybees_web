889a2358db11f52038c08e9ba9a43326461d135e
bluesnooker123 <dev333perfect@gmail.com>
2024-07-17 16:53:09 +0800

    show submit button on Quiz page

--- knowyourself.js
+++ knowyourself.js
@@ -15,8 +15,26 @@ $(document).ready(async function () {
     path: "./HB_assest/quiz.json", // the path to the animation json
   });
 
+  var animation3 = lottie.loadAnimation({
+    container: document.getElementById("lottie-animation3"), // the dom element
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "./HB_assest/quiz.json", // the path to the animation json
+  });
+
+  var animation4 = lottie.loadAnimation({
+    container: document.getElementById("lottie-animation4"), // the dom element
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "./HB_assest/quiz.json", // the path to the animation json
+  });
+
   setTimeout(() => {
     animation.play();
     animation2.play();
+    animation3.play();
+    animation4.play();
   }, 2000);
 });
