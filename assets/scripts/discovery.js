1e4ff3e818b9740d71c17c0b1859601cf0356221
bluesnooker123 <dev333perfect@gmail.com>
2024-07-17 22:32:54 +0800

    update style for radio buttons on Quiz Questions page

--- assets/scripts/discovery.js
+++ assets/scripts/discovery.js
@@ -1,4 +1,19 @@
 $(document).ready(async function() {
+  document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+    radio.addEventListener('change', function() {
+      const name = this.name; // Get the name attribute of the selected radio button
+      // Get all radio buttons with the same name
+      const radios = document.querySelectorAll(`input[name="${name}"]`);
+      radios.forEach((r) => {
+        if (r.checked) {
+          r.parentNode.classList.remove('inactive');
+        } else {
+          r.parentNode.classList.add('inactive');
+        }
+      });
+    });
+  });
+
   setTimeout(function () {
     $('#music_icon_description').addClass('hidden');
   }, 3000);
