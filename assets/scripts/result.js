c755d5fa61d96815500b80ff4004cf97bd0025b5
bluesnooker123 <dev333perfect@gmail.com>
2024-07-18 21:04:05 +0800

    use float-in effect instead of fade-in, update background color on Quiz Result page

--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
       var thisPos = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
       if (topOfWindow + windowHeight - 200 > thisPos ) {
-        $(this).addClass("fade-in-up");
+        $(this).addClass("float-in");
       }
     });
   }  
