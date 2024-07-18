b0644db77c050358e745dc645fa4dfddf2a36c9e
bluesnooker123 <dev333perfect@gmail.com>
2024-07-18 14:32:48 +0800

    initial commit for Quiz Result page

---
+++ assets/scripts/result.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  function showItems(el) {
+    var windowHeight = $(window).height();
+    $(el).each(function(){
+      var thisPos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (topOfWindow + windowHeight - 200 > thisPos ) {
+        $(this).addClass("fade-in-up");
+      }
+    });
+  }  
+
+  // window.scrollTo({
+  //   top: 1,
+  //   behavior: 'smooth'
+  // });
+
+  showItems('.show-item');
+
+  $(window).scroll(function() {
+    showItems('.show-item');
+  });
+ 
+  setTimeout(function () {
+    $('#music_icon_description').addClass('hidden');
+  }, 3000);
+  
+});
+  
