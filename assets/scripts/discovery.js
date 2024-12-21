4675e01023f27215988c47dc382a8bba442c5ee2
bluesnooker123 <dev333perfect@gmail.com>
2024-12-22 07:49:54 +0800

    Rebuild Discovery page, add first question desktop page

--- assets/scripts/discovery.js
+++ assets/scripts/discovery.js
@@ -1,87 +1,74 @@
-$(document).ready(async function () {
-  const question1 = $("#questions-details-wrapper1");
-  question1.removeClass("questions-hidden");
-
-  var progressBar = document.querySelector(".custom-progress .progress-bar");
-  if (progressBar) {
-    progressBar.setAttribute("aria-valuenow", "7");
-    progressBar.style.width = "7%";
-  }
-
-  const sections = [1, 2, 3, 4];
-  sections.forEach(section => {
-    const radios = document.querySelectorAll(`.section${section}`);
-    radios.forEach(radio => {
-      radio.addEventListener('change', function () {
-        checkSectionCompletion(section);
-      });
-    });
-  });
-
-  function checkSectionCompletion(section) {
-    const questions = document.querySelectorAll(`#questions-details-wrapper${section} .questions-detail`);
-    let allChecked = true;
-
-    questions.forEach(question => {
-      const radios = question.querySelectorAll(`input[type="radio"]`);
-      if (!Array.from(radios).some(radio => radio.checked)) {
-        allChecked = false;
-      }
-    });
-
-    const button = document.getElementById(section === 4 ? 'submitButton' : `nextButton${section}`);
-    button.disabled = !allChecked;
-    if ( !allChecked )
-      button.classList.add('disabled-btn')
-    else
-      button.classList.remove('disabled-btn')
-  }
-
-  document.querySelectorAll('input[type="radio"]').forEach((radio) => {
-    radio.addEventListener("change", function () {
-      const question_details = document.querySelectorAll(`.questions-detail`);
-      if ( question_details[Number(this.id.split('_')[0])] && (Number(this.id.split('_')[0]) % 5  !== 0) ) {
-        window.scrollTo({
-          top: question_details[Number(this.id.split('_')[0])].offsetTop,
-          behavior: 'smooth'
-        });
-      }
-
-      const name = this.name; // Get the name attribute of the selected radio button
-      // Get all radio buttons with the same name
-      const radios = document.querySelectorAll(`input[name="${name}"]`);
-      radios.forEach((r) => {
-        if (r.checked) {
-          r.parentNode.classList.remove("inactive");
-        } else {
-          r.parentNode.classList.add("inactive");
-        }
-      });
-    });
-  });
-
-  const firstNameInput = $("#first_name");
-  const lastNameInput = $("#last_name");
-  const mobileInput = $("#mobile");
-  const emailInput = $("#email");
-  const startBtn = $(".start-btn-wrapper");
-
-  function checkInputs() {
-    if (firstNameInput.val().trim() !== "" && lastNameInput.val().trim() !== "" && mobileInput.val().trim() !== "" && emailInput.val().trim() !== "") {
-      startBtn.removeClass("hide");
-      startBtn.addClass("visible");
-    } else {
-      startBtn.removeClass("visible");
-      startBtn.addClass("hide");
-    }
-  }
-
-  firstNameInput.on("input", checkInputs);
-  lastNameInput.on("input", checkInputs);
-  mobileInput.on("input", checkInputs);
-  emailInput.on("input", checkInputs);
-
-  setTimeout(function () {
-    $("#music_icon_description").addClass("fadeOut-scale");
-  }, 3000);
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    window.scrollTo(0, 0); // Force scroll reset
+  }, 10);
 });
+
+const Choice = {
+  A: 'a',
+  NON_SELECT: 'non-select',
+  B: 'b'
+};
+var current_step = 1;
+var choices = new Array(20).fill(Choice.NON_SELECT);
+
+const discoveryCards = document.querySelectorAll('.discovery-card');
+
+discoveryCards.forEach(card => {
+  card.addEventListener('click', function () {
+    const next_button = document.getElementById('nextButton');
+    if ( current_step < 20 ) {
+      next_button.disabled = false;
+      next_button.classList.remove('disabled-btn')
+    }
+    if ( this.id === 'discovery_desktop_1_card_a' ) {
+      choices[0] = Choice.A;
+      if ( !this.classList.contains('flipped') ) {
+        this.classList.toggle('flipped');
+        document.getElementById('discovery_desktop_1_card_b').classList.remove('flipped');
+      }
+    } else if ( this.id === 'discovery_desktop_1_card_b' ) {
+      choices[0] = Choice.B;
+      if ( !this.classList.contains('flipped') ) {
+        this.classList.toggle('flipped');
+        document.getElementById('discovery_desktop_1_card_a').classList.remove('flipped');
+      }
+    }
+  });
+});
+
+function onPrev() {
+  current_step--;
+  if ( current_step < 1 )
+    current_step = 1;
+  showPrevNextButtons();
+}
+function onNext() {
+  current_step++;
+  if ( current_step > 20 )
+    current_step = 20;
+  showPrevNextButtons();
+}
+
+function showPrevNextButtons() {
+  if ( current_step === 1 ) {
+    $("#prevButton").css("visibility", "hidden");
+  } else if ( current_step === 20 ) {
+    $("#nextButton").css("visibility", "hidden");
+  } else {
+    $("#prevButton").css("visibility", "visible");
+    $("#nextButton").css("visibility", "visible");
+  }
+
+  if ( choices[current_step-1] === Choice.NON_SELECT ) {
+    const next_button = document.getElementById('nextButton');
+    next_button.disabled = true;
+    next_button.classList.add('disabled-btn')
+  }
+}
+
+showPrevNextButtons();
+
+$(document).ready(async function () {
+});
+
